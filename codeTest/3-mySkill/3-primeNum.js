87592be8f3f1657b98d0455c038bbb2e78b2d9c8
name7777 <mywonhyuni@gmail.com>
2022-04-19 08:43:23 +0000

    220419_goorm

--- codeTest/3-mySkill/3-primeNum.js
+++ codeTest/3-mySkill/3-primeNum.js
@@ -1,44 +1,47 @@
-function primeNum(a, b, c) {
-    let count = 0;
-    
-    for(var i = 2; i <= a; i++) {
-        if(a % i === 0) {
-            console.log(false);
+/*
+    <문제>
+    주어진 숫자 중 3개의 수를 더했을 때 소수가 되는 경우의 개수를 구하려고 합니다.
+    숫자들이 들어있는 배열 nums가 매겨변수로 주어질 때, nums에 있는 숫자들 중
+    서로 다른 3개를 골라 더했을 때 소수가 되는 경우의 개수를 return 하도록 함수를 완성하라.
+
+    제한사항
+    - nums에 들어있는 숫자의 개수를 3개 이상 50개 이하
+    - nums의 각 원소는 1 이상 1,000 이하의 자연수이며, 중복된 숫자는 들어있지 않다
+
+
+    <접근 방법>
+    - 가장 먼저 이렇게 문제가 길 때는 최대한 쪼개서 함수든 배열이든 나눠서 생각한다
+*/
+
+solution([1, 2, 3, 4]);
+
+function checkPrimeNum(num) {
+    for(let i = 2; i < num; i++) {
+        if (num % i == 0) {
+            return false;
         }
-        else count += 1;
     }
-
-    // for(var i = 2; i <= a+b; i++) {
-    //     if(a % i === 0) {
-    //         console.log(false);
-    //     }
-    //     else count += 1;
-    // }
-
-    // for(var i = 2; i <= a+c; i++) {
-    //     if(a % i === 0) {
-    //         console.log(false);
-    //     }
-    //     else count += 1;
-    // }
-
-    // for(var i = 2; i <= b+c; i++) {
-    //     if(a % i === 0) {
-    //         console.log(false);
-    //     }
-    //     else count += 1;
-    // }
-
-    // for(var i = 2; i <= a+b+c; i++) {
-    //     if(a % i === 0) {
-    //         console.log(false);
-    //     }
-        // else count += 1;
-    // }
-
     
-
-    console.log("count : ", count);
+    return true;
 }
 
-primeNum(2, 3, 4);
+function solution(nums) {
+    let sumNumber = 0;
+    let result = 0;
+
+    // 1) 숫자 n개에서 3개 씩 더하는 코드
+    for(let i = 0; i < nums.length; i++) {
+       for(let j = i + 1; j < nums.length; j++) {
+           for(let k = j + 1; k < nums.length; k++) {
+               sumNumber = nums[i] + nums[j] + nums[k];
+               if (checkPrimeNum(sumNumber)) {
+                   result++;
+               }
+           }
+       }
+    }
+
+    // 2) 그 숫자를 소수인지 판단하는 코드
+    console.log(result);
+    return result;
+}
